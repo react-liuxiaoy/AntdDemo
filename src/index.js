e4c3aa3b2a7bd6534f7dd5c499c1ab97f7f842f3
liuxy <938651469@qq.com>
2018-03-21 10:45:39 +0200

    use antd LocaleProvider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { LocaleProvider } from 'antd';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<LocaleProvider locale={zh_CN}><App /></LocaleProvider>,
+    document.getElementById('root'));
 registerServiceWorker();
