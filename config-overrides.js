32c2129937ff6e804af5fb90115ae8ce04a39768
liuxy <938651469@qq.com>
2018-03-21 04:14:27 +0200

    use react-app-rewired

---
+++ config-overrides.js
@@ -0,0 +1,7 @@
+/* Copyright (c) 2016-2018. Suzhou DHMS Information Technology Co.,Ltd.
+ * Author: liuxy Created:03/21/2018
+ */
+module.exports = function override(config, env) {
+    // do stuff with the webpack config...
+    return config;
+};
