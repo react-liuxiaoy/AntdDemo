d72f3075517e42b1367024a725c204ac493c8e0b
liuxy <938651469@qq.com>
2018-03-21 04:29:17 +0200

    use react-app-rewire-less

--- config-overrides.js
+++ config-overrides.js
@@ -2,12 +2,17 @@
  * Author: liuxy Created:03/21/2018
  */
 const {injectBabelPlugin} = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
 module.exports = function override(config, env) {
     // do stuff with the webpack config...
     config = injectBabelPlugin(
         ['import',
-            {libraryName: 'antd', libraryDirectory: 'es', style: 'css'}
+            {libraryName: 'antd', libraryDirectory: 'es', style: true}
         ],
         config);
+    config = rewireLess.withLoaderOptions({
+        modifyVars: { "@primary-color": "#1DA57A" },
+    })(config, env);
     return config;
 };
